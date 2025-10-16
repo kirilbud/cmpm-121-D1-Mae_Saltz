d0d99cb3cfe55369cbfc36ab86011167347a8cd3
Mae Saltz <76541897+kirilbud@users.noreply.github.com>
2025-10-16 23:34:38 +0000

    added button and text for upgrades but have not implemented them yet

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,17 @@ let delta_update: number = performance.now() / 1000.0;
 let delta_time: number = 0;
 
 let woodCounter: number = 0;
+//let _woodCutters: number = 0;
+//let _woodPerSec: number = 0;
 
 // Create basic HTML structure
 document.body.innerHTML = `
   <h1>Destruction of the earth!</h1>
   <button id="wood_increment">Chop Wood!</button>
   <div><p>Wood: <span id="wood_counter">0</span></p></div>
+  
+  <div><p>You currently own: <span id="wood_cutters">0</span> automatic wood cutters giving you <span id="woodPerSec">0</span> wood per sec </p></div>
+  <button id="wood_increment">buy a wood cutter for <span id="wood_counters">10</span> wood!</button>
 `;
 
 // Add click handler
