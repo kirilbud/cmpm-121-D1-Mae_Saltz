98026d88c5d5d6cc05407df67c5783623c81973a
Mae Saltz <76541897+kirilbud@users.noreply.github.com>
2025-11-04 01:33:35 +0000

    Added fishing and emissions

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,10 @@ document.body.innerHTML = `
   <button id="stone_increment">Mine Stone!⛰️</button>
   <div><p>Wood: <span id="wood_counter">0</span></p></div>
   <div><p>Stone: <span id="stone_counter">0</span></p></div>
-  <div><p>Your buildings are giving you <span id="wood_persec">0</span> wood per second and <span id="stone_persec">0</span> stone per second </p></div>
-  
-
-
+  <div><p>Emissions: <span id="emission_counter">0</span></p></div>
+  <div><p>Fish left in the world: <span id="fish_counter">3500000000000</span> out of 3500000000000</p></div>
+  <div><p>Your buildings are giving you <span id="wood_persec">0</span> wood per second, <span id="stone_persec">0</span> stone per second and catching <span id="fish_persec">0</span> fish per second </p></div>
+  <div id="every-fish"></div>
 `;
 //represents a building and or upgrade
 interface Item {
@@ -88,7 +88,7 @@ const availableItems: Item[] = [
   {
     name: "small cave",
     cost: 100,
-    costType: "",
+    costType: "wood",
     rate: 1.25,
     amount: 0,
     button: tempButton,
@@ -113,6 +113,47 @@ const availableItems: Item[] = [
       "not mining for gemstones or rare metals no you are here for 100 stone per second",
     descriptionElement: tempPElem,
   },
+  {
+    name: "fishing rod",
+    cost: 10,
+    costType: "emissions",
+    rate: 1.5,
+    amount: 0,
+    button: tempButton,
+    productionType: "fish",
+    production: 10,
+    description: "",
+    buttonPrompt: "humble fishing rod giving you 10 fish per second",
+    descriptionElement: tempPElem,
+  },
+  {
+    name: "fishing boat",
+    cost: 100,
+    costType: "emissions",
+    rate: 1.5,
+    amount: 0,
+    button: tempButton,
+    productionType: "fish",
+    production: 100,
+    description: "",
+    buttonPrompt:
+      "boats of fisherman fishing away giving you 100 fish per second",
+    descriptionElement: tempPElem,
+  },
+  {
+    name: "auto fisher 9000",
+    cost: 1000,
+    costType: "emissions",
+    rate: 1.5,
+    amount: 0,
+    button: tempButton,
+    productionType: "fish",
+    production: 1000,
+    description: "",
+    buttonPrompt:
+      "the newest in fishing technology guaranteed to kill every fish eventually giving you 1000 fish per second",
+    descriptionElement: tempPElem,
+  },
 ];
 
 //GLOBALS
@@ -128,13 +169,25 @@ const stoneCounterElement = document.getElementById("stone_counter")!;
 
 const stonePerSec = document.getElementById("stone_persec")!;
 
+const fishCounterElement = document.getElementById("fish_counter")!;
+const fishPerSec = document.getElementById("fish_persec")!;
+
+const emissionsCounterElement = document.getElementById("emission_counter")!;
+
+const everyFish = document.getElementById("every-fish")!;
+
 //time passed from last frame
 let delta_update: number = performance.now() / 1000.0;
 let delta_time: number = 0;
 
+//acording to https://www.worldatlas.com/articles/how-many-fish-are-there-in-the-ocean.html
+const maxFish: number = 3500000000000;
+
 //resources available
 let woodCounter: number = 0;
 let stoneCounter: number = 0;
+let emissionsCounter: number = 0;
+let fishCounter: number = maxFish;
 
 //button functions
 woodButton.addEventListener("click", () => {
@@ -158,6 +211,39 @@ function increment_stone(amount: number) {
   stoneCounterElement.textContent = String(Math.floor(stoneCounter));
 }
 
+let win: boolean = false;
+function increment_fish(amount: number) {
+  fishCounter = fishCounter + amount * delta_time;
+  if (fishCounter < 0 && !win) {
+    fishCounter = 0;
+    win = true;
+    //<iframe width="560" height="235" src="https://www.youtube.com/embed/aWBpBjdp8Hg?si=Efnj3rZiszl_k2ws&amp;start=107" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    const video = document.createElement("iframe");
+    video.setAttribute("width", "560");
+    video.setAttribute("height", "235");
+    video.setAttribute(
+      "src",
+      "https://www.youtube.com/embed/aWBpBjdp8Hg?start=107&si=Efnj3rZiszl_k2ws",
+    );
+    video.setAttribute("frameborder", "0");
+    video.setAttribute(
+      "allow",
+      "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share",
+    );
+    video.setAttribute("referrerpolicy", "strict-origin-when-cross-origin");
+    video.setAttribute("allowfullscreen", "");
+    everyFish.appendChild(video);
+  } else if (fishCounter < 0) {
+    fishCounter = 0;
+  }
+  fishCounterElement.textContent = String(Math.floor(fishCounter));
+}
+
+function increment_emmisions(amount: number) {
+  emissionsCounter = emissionsCounter + amount * delta_time;
+  emissionsCounterElement.textContent = String(Math.floor(emissionsCounter));
+}
+
 //setup the game
 start();
 function start() {
@@ -170,6 +256,10 @@ function start() {
     const item = document.createElement("button");
     item.textContent = "buy a " + upgrade.name + " for " + upgrade.cost + " " +
       upgrade.costType + "!";
+    if (upgrade.costType == "emissions") {
+      item.textContent = "buy a " + upgrade.name + " at " + upgrade.cost +
+        " " + upgrade.costType + "!";
+    }
     item.id = upgrade.name;
     upgrade.descriptionElement = itemDescription;
     item.title = upgrade.buttonPrompt;
@@ -180,12 +270,18 @@ function start() {
       upgrade.amount = upgrade.amount + 1;
       if (upgrade.costType == "wood") {
         woodCounter = woodCounter - upgrade.cost;
-      } else {
+      } else if (upgrade.costType == "stone") {
         stoneCounter = stoneCounter - upgrade.cost;
       }
       upgrade.cost = Math.floor(upgrade.cost * upgrade.rate);
       item.textContent = "buy a " + upgrade.name + " for " + upgrade.cost +
         " " + upgrade.costType + "!";
+
+      if (upgrade.costType == "emissions") {
+        item.textContent = "buy a " + upgrade.name + " at " + upgrade.cost +
+          " " + upgrade.costType + "!";
+      }
+
       upgrade.descriptionElement.textContent = "you currently own " +
         upgrade.amount + " " +
         upgrade.name + "s";
@@ -200,23 +296,33 @@ function tick() {
   delta_time = performance.now() / 1000.0 - delta_update;
   delta_update = performance.now() / 1000.0;
 
+  let toatalBuildings = 0;
   let woodPerSecs = 0;
   let stonePerSecs = 0;
+  let fishPerSecs = 0;
   for (const upgrade of availableItems) {
     if (upgrade.costType == "wood") {
       upgrade.button.disabled = upgrade.cost > woodCounter;
-    } else {
+    } else if (upgrade.costType == "stone") {
       upgrade.button.disabled = upgrade.cost > stoneCounter;
+    } else {
+      upgrade.button.disabled = upgrade.cost > emissionsCounter;
     }
     if (upgrade.productionType == "wood") {
       woodPerSecs = woodPerSecs + upgrade.amount * upgrade.production;
-    } else {
+    } else if (upgrade.productionType == "stone") {
       stonePerSecs = stonePerSecs + upgrade.amount * upgrade.production;
+    } else {
+      fishPerSecs = fishPerSecs + upgrade.amount * upgrade.production;
     }
+    toatalBuildings = toatalBuildings + upgrade.amount;
   }
   increment_stone(stonePerSecs);
   increment_wood(woodPerSecs);
+  increment_emmisions(toatalBuildings);
+  increment_fish(-fishPerSecs);
   woodPerSec.textContent = String(woodPerSecs);
   stonePerSec.textContent = String(stonePerSecs);
+  fishPerSec.textContent = String(fishPerSecs);
   requestAnimationFrame(tick);
 }
